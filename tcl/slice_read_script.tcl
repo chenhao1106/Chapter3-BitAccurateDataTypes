14fd85565ed8ef5869aa116cb1dbf49b7a6e215b
chenhao1106 <49955647+chenhao1106@users.noreply.github.com>
2021-04-12 21:06:48 +0800

    tcl file upload

---
+++ tcl/slice_read_script.tcl
@@ -0,0 +1,18 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2019 Xilinx, Inc. All Rights Reserved.
+############################################################
+open_project LABA
+set_top slice_read
+add_files LABA/slice_read.cpp
+add_files slice_read.h
+add_files -tb LABA/slice_read_tb.cpp
+open_solution "solution1"
+set_part {xc7z020clg400-1}
+create_clock -period 5 -name default
+source "./LABA/solution1/directives.tcl"
+csim_design
+csynth_design
+cosim_design -trace_level all -tool xsim
+export_design -format ip_catalog
